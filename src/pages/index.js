8a66e20170ed53657ee129587c4f05793a110350
mariekonopacki <31550539+mariekonopacki@users.noreply.github.com>
2019-03-06 01:49:36 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import _ from 'lodash'
 
 import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
+
 import Layout from '../components/layout'
 import Piecepreviewrow from '../components/piecepreviewrow'
 import Circlebutton from '../components/circlebutton'
@@ -9,13 +11,21 @@ import Featuredcolumn from '../components/featuredcolumn'
 import Shorten from '../components/shorten'
 import Shortenfeatured from '../components/shortenfeatured'
 import SEO from '../components/seo'
-import Navbar from '../components/navbar'
+import Navbar from '../components/sidenav'
 
 
-export default ({data }) => {
-  let featuredSample = _.sampleSize(data.all.edges, 3)
-  let artSample = _.sampleSize(data.art.edges, 4)
+class Index extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.featuredSample = _.sampleSize(props.data.all.edges, 3)
+    this.artSample = _.sampleSize(props.data.art.edges, 4)
+    this.essaySample = _.sampleSize(props.data.personalessay.edges, 3)
+    this.fictionSample = _.sampleSize(props.data.fiction.edges, 3)
+    this.poetrySample = _.sampleSize(props.data.poetry.edges, 3)
+  }
+
+  render() {
   return (
     <Layout>
       <SEO title="Home" keywords={[`literary`, `art`, `magazine`]} />
@@ -33,7 +43,7 @@ export default ({data }) => {
 
       <h4 class="top">Featured</h4>
       <div class="featuredrow">
-        {featuredSample.map(post => (
+        {this.featuredSample.map(post => (
           <Featuredcolumn>
             <div key={post.node.id}>
               <a href={post.node.frontmatter.path}>
@@ -52,44 +62,44 @@ export default ({data }) => {
       </div>
 
       <div class="featuredpiece">
-          <div class="item-1" href={featuredSample[0].node.frontmatter.path}>
-            <a class="featuredLink" href={featuredSample[0].node.frontmatter.path}>
-            <img class="featuredImage" alt="first feature" src={featuredSample[0].node.frontmatter.source.childImageSharp.sizes.src}></img>
+          <div class="item-1" href={this.featuredSample[0].node.frontmatter.path}>
+            <a class="featuredLink" href={this.featuredSample[0].node.frontmatter.path}>
+            <img class="featuredImage" alt="first feature" src={this.featuredSample[0].node.frontmatter.source.childImageSharp.sizes.src}></img>
               <div class="featuredInfo">
-                <h2>{featuredSample[0].node.frontmatter.title}</h2>
+                <h2>{this.featuredSample[0].node.frontmatter.title}</h2>
                 <small>
-                  {featuredSample[0].node.frontmatter.type} by {featuredSample[0].node.frontmatter.author}
+                  {this.featuredSample[0].node.frontmatter.type} by {this.featuredSample[0].node.frontmatter.author}
                 </small>
                 <Shortenfeatured>
-                  {featuredSample[0].node.excerpt}
+                  {this.featuredSample[0].node.excerpt}
                 </Shortenfeatured>
               </div>
             </a>
           </div>
           <div class="item-2">
-            <a class="featuredLink" href={featuredSample[1].node.frontmatter.path}>
-              <img class="featuredImage" alt="second feature" src={featuredSample[1].node.frontmatter.source.childImageSharp.sizes.src}></img>
+            <a class="featuredLink" href={this.featuredSample[1].node.frontmatter.path}>
+              <img class="featuredImage" alt="second feature" src={this.featuredSample[1].node.frontmatter.source.childImageSharp.sizes.src}></img>
               <div class="featuredInfo">
-                <h2>{featuredSample[1].node.frontmatter.title}</h2>
+                <h2>{this.featuredSample[1].node.frontmatter.title}</h2>
                 <small>
-                  {featuredSample[1].node.frontmatter.type} by {featuredSample[1].node.frontmatter.author}
+                  {this.featuredSample[1].node.frontmatter.type} by {this.featuredSample[1].node.frontmatter.author}
                 </small>
                 <Shortenfeatured>
-                  {featuredSample[1].node.excerpt}
+                  {this.featuredSample[1].node.excerpt}
                 </Shortenfeatured>
               </div>
             </a>
           </div>
           <div class="item-3">
-            <a class="featuredLink" href={featuredSample[2].node.frontmatter.path}>
-            <img class="featuredImage" alt="third feature" src={featuredSample[2].node.frontmatter.source.childImageSharp.sizes.src}></img>
+            <a class="featuredLink" href={this.featuredSample[2].node.frontmatter.path}>
+            <img class="featuredImage" alt="third feature" src={this.featuredSample[2].node.frontmatter.source.childImageSharp.sizes.src}></img>
               <div class="featuredInfo">
-                <h2>{featuredSample[2].node.frontmatter.title}</h2>
+                <h2>{this.featuredSample[2].node.frontmatter.title}</h2>
                 <small>
-                  {featuredSample[2].node.frontmatter.type} by {featuredSample[2].node.frontmatter.author}
+                  {this.featuredSample[2].node.frontmatter.type} by {this.featuredSample[2].node.frontmatter.author}
                 </small>
                 <Shortenfeatured>
-                  {featuredSample[2].node.excerpt}
+                  {this.featuredSample[2].node.excerpt}
                 </Shortenfeatured>
               </div>
             </a>
@@ -100,7 +110,7 @@ export default ({data }) => {
 
       <h4>Explore all</h4>
       <Piecepreviewrow>
-        {_.sampleSize(data.fiction.edges, 3).map(post => (
+        {this.fictionSample.map(post => (
           <a href={post.node.frontmatter.path}>
             <div class="piecepreviewcolumn">
               <div class="piecepreview fictionpreview">
@@ -127,7 +137,7 @@ export default ({data }) => {
 
         <div class="artSelection">
           <div class="artpreviewrow">
-            {artSample.map(post => (
+            {this.artSample.map(post => (
                 <a href={post.node.frontmatter.path}>
                   <div class="artTitleArtist" key={post.node.id}>
                     <h3>{post.node.frontmatter.title}</h3>
@@ -144,23 +154,23 @@ export default ({data }) => {
           <div class="rightmostArt">
             <div class="artpiece">
               <div class="artitem-1">
-                <a href={artSample[0].node.frontmatter.path}>
-                  <div class="artDisplay"><img alt={artSample[0].node.frontmatter.title} src={artSample[0].node.frontmatter.source.childImageSharp.sizes.src} /></div>
+                <a href={this.artSample[0].node.frontmatter.path}>
+                  <div class="artDisplay"><img alt={this.artSample[0].node.frontmatter.title} src={this.artSample[0].node.frontmatter.source.childImageSharp.sizes.src} /></div>
                 </a>
               </div>
               <div class="artitem-2">
-                <a href={artSample[1].node.frontmatter.path}>
-                  <div class="artDisplay"><img alt={artSample[1].node.frontmatter.title} src={artSample[1].node.frontmatter.source.childImageSharp.sizes.src} /></div>
+                <a href={this.artSample[1].node.frontmatter.path}>
+                  <div class="artDisplay"><img alt={this.artSample[1].node.frontmatter.title} src={this.artSample[1].node.frontmatter.source.childImageSharp.sizes.src} /></div>
                 </a>
               </div>
               <div class="artitem-3">
-                <a href={artSample[2].node.frontmatter.path}>
-                  <div class="artDisplay"><img alt={artSample[2].node.frontmatter.title} src={artSample[2].node.frontmatter.source.childImageSharp.sizes.src} /></div>
+                <a href={this.artSample[2].node.frontmatter.path}>
+                  <div class="artDisplay"><img alt={this.artSample[2].node.frontmatter.title} src={this.artSample[2].node.frontmatter.source.childImageSharp.sizes.src} /></div>
                 </a>
               </div>
               <div class="artitem-4">
-                <a href={artSample[3].node.frontmatter.path}>
-                  <div class="artDisplay"><img alt={artSample[3].node.frontmatter.title} src={artSample[3].node.frontmatter.source.childImageSharp.sizes.src} /></div>
+                <a href={this.artSample[3].node.frontmatter.path}>
+                  <div class="artDisplay"><img alt={this.artSample[3].node.frontmatter.title} src={this.artSample[3].node.frontmatter.source.childImageSharp.sizes.src} /></div>
                 </a>
               </div>
             </div>
@@ -171,7 +181,7 @@ export default ({data }) => {
 
 
       <Piecepreviewrow>
-        {_.sampleSize(data.personalessay.edges, 3).map(post => (
+        {this.essaySample.map(post => (
           <a href={post.node.frontmatter.path}>
           <div class="piecepreviewcolumn">
             <div class="piecepreview essaypreview">
@@ -195,7 +205,7 @@ export default ({data }) => {
       </Piecepreviewrow>
 
       <Piecepreviewrow>
-        {_.sampleSize(data.poetry.edges, 3).map(post => (
+        {this.poetrySample.map(post => (
           <a href={post.node.frontmatter.path}>
             <div class="piecepreviewcolumn">
               <div class="piecepreview poetrypreview">
@@ -219,11 +229,11 @@ export default ({data }) => {
       </Piecepreviewrow>
     </Layout>
 
-  )
+  )}
 }
 
 
-
+export default Index;
 
 export const pieceQuery = graphql`
     query PieceIndexQuery {

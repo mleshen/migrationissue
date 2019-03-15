632c63c19f47482bcc6d35c5c445670de9b6afaf
Marie Konopacki <mariekonopacki@college.harvard.edu>
2019-03-15 14:16:37 -0400

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { Link } from 'gatsby'
 export default ({ children }) => (
   <div class="footer">
     <small>
-      Copyright © The Women's Issue 2019 &nbsp;|&nbsp; <a href="https://theharvardadvocate.com/shop/9">Buy the Issue</a> &nbsp;|&nbsp; <a href="/about">About the Issue</a> &nbsp;|&nbsp; <a href="/">Home</a>
+      Copyright © The Women's Issue 2019 &nbsp;|&nbsp; <a href="/">Home</a> &nbsp|&nbsp; <a href="https://theharvardadvocate.com/shop/9">Buy the Issue</a> &nbsp;|&nbsp; <a href="/about">About the Issue</a> &nbsp;
     </small>
 
   </div>

82067ab5c84433ec79b578a53cb8df6d84d24667
Marie Konopacki <mariekonopacki@college.harvard.edu>
2019-03-25 17:36:46 -0400

    fix order

--- src/pages/index.js
+++ src/pages/index.js
@@ -173,6 +173,41 @@ class Index extends React.Component {
 
 
         <Piecepreviewrow>
+          {essaySample.map(post => (
+            <a href={post.node.frontmatter.path}>
+              <div class="piecepreviewcolumn">
+                <div key={post.node.id} class="piecepreview essaypreview">
+                    <div class="previewDefault">
+                      <img class="previewImage" src={post.node.frontmatter.source.childImageSharp.sizes.src}></img>
+                      <div class="previewInfo">
+                        <h3>{post.node.frontmatter.title}</h3>
+                        <small>
+                          {post.node.frontmatter.type} by {post.node.frontmatter.author}
+                        </small>
+                      </div>
+                    </div>
+
+                    <div class="previewOverlay">
+                      <div class="overlayContent">
+                        <h3>{post.node.frontmatter.title}</h3>
+                        <small>
+                          {post.node.frontmatter.type} by {post.node.frontmatter.author}
+                        </small>
+                        <Shorten>{post.node.excerpt}</Shorten><br />
+                        <div class="fadetowhite"></div>
+                        <Circlebutton> <small> Read more </small> </Circlebutton>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </a>
+          ))}
+          <a href="/personalessay"><h4 class="seemore">See more personal essays </h4></a>
+        </Piecepreviewrow>
+
+        <div class="linebreak"></div>
+
+        <Piecepreviewrow>
           {poetrySample.map(post => (
               <div class="piecepreviewcolumn">
 
@@ -207,42 +242,6 @@ class Index extends React.Component {
               </div>
           ))}
           <a href="/poetry"><h4 class="seemore">See more poetry </h4></a>
-        </Piecepreviewrow>
-
-        <div class="linebreak"></div>
-
-
-        <Piecepreviewrow>
-          {essaySample.map(post => (
-            <a href={post.node.frontmatter.path}>
-              <div class="piecepreviewcolumn">
-                <div key={post.node.id} class="piecepreview essaypreview">
-                    <div class="previewDefault">
-                      <img class="previewImage" src={post.node.frontmatter.source.childImageSharp.sizes.src}></img>
-                      <div class="previewInfo">
-                        <h3>{post.node.frontmatter.title}</h3>
-                        <small>
-                          {post.node.frontmatter.type} by {post.node.frontmatter.author}
-                        </small>
-                      </div>
-                    </div>
-
-                    <div class="previewOverlay">
-                      <div class="overlayContent">
-                        <h3>{post.node.frontmatter.title}</h3>
-                        <small>
-                          {post.node.frontmatter.type} by {post.node.frontmatter.author}
-                        </small>
-                        <Shorten>{post.node.excerpt}</Shorten><br />
-                        <div class="fadetowhite"></div>
-                        <Circlebutton> <small> Read more </small> </Circlebutton>
-                      </div>
-                    </div>
-                </div>
-              </div>
-            </a>
-          ))}
-          <a href="/personalessay"><h4 class="seemore">See more personal essays </h4></a>
         </Piecepreviewrow>
 
         <div class="linebreak"></div>
